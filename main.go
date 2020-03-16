22405a84a6eca292df875ee32d46c0c56692c408
Sam Tholiya <tholiya.sam@outlook.in>
2020-03-16 16:34:59 +0530

    Added trace level logging

--- main.go
+++ main.go
@@ -24,13 +24,19 @@ import (
 var log *logrus.Logger
 
 func main() {
-	isDebugMode := flag.Bool("debug", false, "Run debug level")
+	isDebugMode := flag.Bool("debug", false, "Run in debug log level")
+	isTraceMode := flag.Bool("trace", false, "Run in trace log level")
 	isProxyServer := flag.Bool("proxy", false, "Run in proxy server mode")
 	host := flag.String("host", "https://httpbin.org", "Server url for proxy server")
 	port := flag.String("port", "3000", "Port number for the server")
 	flag.Parse()
-	if *isDebugMode || strings.Compare(common.GetEnv("M_DEBUG_LEVEL", "INFO"), "DEBUG") == 0 {
+	if *isDebugMode || strings.Compare(common.GetEnv("M_LOG_LEVEL", "INFO"), "DEBUG") == 0 {
+		common.GetLogger().Info("Working in debug mode")
 		common.GetLogger().SetLevel(logrus.DebugLevel)
+	}
+	if *isTraceMode || strings.Compare(common.GetEnv("M_LOG_LEVEL", "INFO"), "TRACE") == 0 {
+		common.GetLogger().Info("Working in trace mode")
+		common.GetLogger().SetLevel(logrus.TraceLevel)
 	}
 	if *isProxyServer || strings.Compare(common.GetEnv("M_PROXY", "false"), "true") == 0 {
 		startProxyServer(common.GetEnv("M_HOST_URL", *host), common.GetEnv("M_PORT", *port))
